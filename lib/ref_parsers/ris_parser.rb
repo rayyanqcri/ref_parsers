d8a0d9a9216fa403e7bc71aba7ce605ef2f5cd1e
Hossam Hammady <github@hammady.net>
2014-09-10 16:35:32 +0300

    supporting more variations of entry codes

--- lib/ref_parsers/ris_parser.rb
+++ lib/ref_parsers/ris_parser.rb
@@ -5,7 +5,7 @@ module RefParsers
       @type_key = "TY"
       @types = %w(ABST ADVS ART BILL BOOK CASE CHAP COMP CONF CTLG DATA ELEC GEN HEAR ICOMM INPR JFULL JOUR MAP MGZN MPCT MUSIC NEWS PAMP PAT PCOMM RPRT SER SLIDE SOUND STAT THES UNBILl UNPB VIDEO)
       @terminator_key = "ER"
-      @line_regex = /^(TY|ID|T1|TI|CT|A1|A2|AU|Y1|PY|N1|KW|RP|SP|EP|JF|JO|JA|J1|J2|VL|IS|T2|CY|PB|U1|U5|T3|N2|SN|AV|M1|M3|AD|UR|L1|L2|L3|L4|ER)  -( (.*))?$/
+      @line_regex = /^(TY|ID|T1|TI|CT||BT|A1|A2|A3|A4|AU|Y1|Y2|Y3|PY|N1|KW|RP|SP|EP|JF|JO|JA|J1|J2|VL|IS|CP|T2|CY|PB|U1|U5|T3|N2|AB|SN|AV|M1|M3|AD|UR|L1|L2|L3|L4|LA|ER)  -( (.*))?$/
       @key_regex_order = 1
       @value_regex_order = 3
       @regex_match_length = 4
--- lib/ref_parsers/version.rb
+++ lib/ref_parsers/version.rb
@@ -1,3 +1,3 @@
 module RefParsers
-  VERSION = "0.0.3"
+  VERSION = "0.0.4"
 end
