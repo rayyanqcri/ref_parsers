6e1575b93ae4ea2bca71aefb2461cbbc708f83af
Hossam Hammady <github@hammady.net>
2015-03-26 15:56:18 +0300

    covering a lot more RIS tags, bumping version to 0.0.7

--- lib/ref_parsers/ris_parser.rb
+++ lib/ref_parsers/ris_parser.rb
@@ -5,7 +5,7 @@ module RefParsers
       @type_key = "TY"
       @types = %w(ABST ADVS ART BILL BOOK CASE CHAP COMP CONF CTLG DATA ELEC GEN HEAR ICOMM INPR JFULL JOUR EJOUR MAP MGZN MPCT MUSIC NEWS PAMP PAT PCOMM RPRT SER SLIDE SOUND STAT THES UNBILl UNPB VIDEO)
       @terminator_key = "ER"
-      @line_regex = /^(TY|ID|T1|TI|CT||BT|A1|A2|A3|A4|AU|Y1|Y2|Y3|PY|N1|KW|RP|SP|EP|JF|JO|JA|J1|J2|VL|IS|CP|T2|CY|PB|U1|U5|T3|N2|AB|SN|AV|M1|M3|AD|UR|L1|L2|L3|L4|LA|ER)  -( (.*))?$/
+      @line_regex = /^(A1|A2|A3|A4|AB|AD|AN|AU|AV|BT|C1|C2|C3|C4|C5|C6|C7|C8|CA|CN|CP|CT|CY|DA|DB|DO|DP|EP|ER|ET|ID|IS|J1|J2|JA|JF|JO|KW|L1|L2|L3|L4|LA|LB|M1|M2|M3|N1|N2|NV|OP|PB|PY|RI|RN|RP|SE|SN|SP|ST|SV|T1|T2|T3|TA|TI|TT|TY|U1|U5|UR|VL|Y1|Y2|Y3)  -( (.*))?$/
       @key_regex_order = 1
       @value_regex_order = 3
       @regex_match_length = 4
--- lib/ref_parsers/version.rb
+++ lib/ref_parsers/version.rb
@@ -1,3 +1,3 @@
 module RefParsers
-  VERSION = "0.0.6"
+  VERSION = "0.0.7"
 end
