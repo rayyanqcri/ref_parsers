65369d3c5a2b661363187bc597df198ddf0fa854
Fikrisaoudi <saoudifikri@hotmail.fr>
2017-04-05 14:07:17 +0300

    Update ref_parsers.rb

--- lib/ref_parsers.rb
+++ lib/ref_parsers.rb
@@ -3,6 +3,8 @@ require "ref_parsers/line_parser"
 require "ref_parsers/ris_parser"
 require "ref_parsers/endnote_parser"
 require "ref_parsers/pubmed_parser"
+require "ref_parsers/ISIparser"
+
 
 module RefParsers
 end
