fc9d5f9f2ca7154b85a4626b42e350bc885a4264
Hossam Hammady <github@hammady.net>
2017-04-16 14:15:48 +0300

    Now supporting optional header and footer using regex

--- lib/ref_parsers/line_parser.rb
+++ lib/ref_parsers/line_parser.rb
@@ -9,7 +9,7 @@ module RefParsers
     def parse(body)
       lines = body.split(/\n\r|\r\n|\n|\r/)
       entries = []
-      next_line = 0
+      next_line = skip_header(lines)
       begin
         entry_found = false
         next_line = parse_entry(lines, next_line) do |entry|
@@ -22,12 +22,30 @@ module RefParsers
 
 protected
 
+    def skip_header(lines)
+      return 0 unless @header_regexes
+      next_line = 0
+      @header_regexes.each do |regex|
+        line = lines[next_line]
+        raise "Header line #{next_line} missing" unless line.match(regex)
+        next_line += 1
+      end
+      next_line
+    end
+
+    def detect_footer(line)
+      return nil unless @footer_regex
+      return {footer: true} if line.match(@footer_regex)
+    end
+
     def parse_entry(lines, next_line)
       begin
         return next_line if next_line >= lines.length
         first = parse_first_line(lines[next_line])
         next_line = next_line + 1
       end while first.nil?
+
+      return if first[:footer]
 
       fields = [first]
 
@@ -36,6 +54,7 @@ protected
         parsed = parse_line(lines[next_line])
         next_line = next_line + 1
         if parsed
+          return if parsed[:footer]
           stop = false
           if parsed[:key] == "-1"
             parsed[:key] = last_parsed[:key]
@@ -59,7 +78,7 @@ protected
 
     def parse_first_line(line)
       first = parse_line(line, /^\d+/)  # skip leading entry numbers
-      return nil if first.nil?
+      return first if first.nil? || first[:footer]
       raise "First line should start with #{@type_key}" if first[:key] != @type_key
       # lets not check for semantics here, leave it for the library client
       # raise "#{line}: Reference type should be one of #{@types.inspect}" unless @types.include? first[:value]
@@ -69,6 +88,8 @@ protected
     def parse_line(line, *ignores)
       ignores << /^\s*$/
       return nil if line.nil? || ignores.any?{|e| line.match(e)}
+      footer = detect_footer(line)
+      return footer if footer
       m = line.match(@line_regex)
       if m && m.length == @regex_match_length
         value = m[@value_regex_order].strip rescue nil
