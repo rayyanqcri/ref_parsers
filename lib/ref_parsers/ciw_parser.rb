55363cfde131e00cc6e9dbe77ddd2903c919d19c
Hossam Hammady <github@hammady.net>
2017-04-24 14:45:00 +0300

    Fixing regexes and more indentation

--- lib/ref_parsers/ciw_parser.rb
+++ lib/ref_parsers/ciw_parser.rb
@@ -2,8 +2,8 @@ module RefParsers
  class CIWParser < LineParser
 
    def initialize
-     @footer_regex = "EF"
-	 @header_regexes =["FN","VR"]
+     @footer_regex = /^EF\s*$/
+     @header_regexes = [/^FN/, /^VR/]
      @type_key = "PT"
      @terminator_key = "ER"
      @line_regex =  /^(AB|AF|AP|AR|AU|BE|BN|BP|C1|CA|CL|CR|CY|CT|DE|DI|DT|ED|EM|EP|ER|EF|EI|FN|FU|FX|GA|HO|ID|IS|J9|JI|LA|NR|OI|PD|PA|PG|PI|PN|PT|PU|PY|PM|RP|RI|SC|SE|SI|SN|SO|SP|SU|TC|TI|UT|U1|U2|UR|VL|VR|WC|WP|Z9|ZB|Z8|ZS)(\s*(.*))?$/
