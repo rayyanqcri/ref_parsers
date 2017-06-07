790c050801b9fb9fee765444dbcedf62e3f246df
Dawar Ahmed <dahmedjetpack@gmail.com>
2017-06-07 10:27:08 +0300

    Completed line_parser_spec and updated spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,14 +20,12 @@
 require 'simplecov'
 require 'coveralls'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new [
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
 ]
 
-SimpleCov.start do
-  add_filter "/spec"
-end
+SimpleCov.start
 
 require 'ref_parsers'
 
