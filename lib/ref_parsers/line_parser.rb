ca66bebcbce5bb21ffc9c755fe2528420c4bfc23
Karim Ahmed <kariem2k@gmail.com>
2018-11-23 18:28:11 +0000

    Inner exception set as a member of the exception.

--- lib/ref_parsers/line_parser.rb
+++ lib/ref_parsers/line_parser.rb
@@ -4,12 +4,13 @@ module RefParsers
   class LineParsingException < StandardError
     attr_reader :line_number
     attr_reader :line_string
+    attr_reader :inner_exception
 
     def initialize(message, line_number, line_string, innerException = nil)
       super("#{message} #{'(See inner exception for details)' if innerException} | Line# #{line_number} | Line Text: #{line_string}")
-      self.set_backtrace(innerException.backtrace) if innerException
       @line_number = line_number
       @line_string = line_string
+      @inner_exception = inner_exception
     end
   end
 
